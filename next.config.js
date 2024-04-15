8a1a175ba769bd58d7421c3fb00b7f6883a6590c
sivani-git <113410457+sivani-git@users.noreply.github.com>
2024-04-15 20:45:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+ 
+  
 };
 
 module.exports = nextConfig;
